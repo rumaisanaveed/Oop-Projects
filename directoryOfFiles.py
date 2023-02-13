abd3253828d356436067145504bb4b70c3b6c84c
Rumaisa Naveed <121342400+rumaisanaveed@users.noreply.github.com>
2023-02-13 22:36:54 +0500

    Update directoryOfFiles.py

--- directoryOfFiles.py
+++ directoryOfFiles.py
@@ -1,24 +1,24 @@
 import os
-##class FilesList():
-##    def __init__(self,p):
-##        self.path = p
-##
-##    def listOfFiles(self):
-##        directory = os.scandir(self.path) # returns files names
-##        for i in directory:
-##            if i.is_file(): # checks if it's a file or not
-##                if i.name.endswith(".txt"): # checks if it's a text file
-##                     print(i.name)
-##                
-##p = "C:\\Users\Humayun\Desktop"        
-##o1 = FilesList(p)
-##o1.listOfFiles()
-##p = "C:\\Users\Humayun\Documents"
-##o2 = FilesList(p)
-##o2.listOfFiles()
-##p = "C:\\SalesData"
-##o3 = FilesList(p)
-##o3.listOfFiles()
+class FilesList():
+   def __init__(self,p):
+       self.path = p
+
+   def listOfFiles(self):
+       directory = os.scandir(self.path) # returns files names
+       for i in directory:
+           if i.is_file(): # checks if it's a file or not
+               if i.name.endswith(".txt"): # checks if it's a text file
+                    print(i.name)
+               
+p = "C:\\Users\Humayun\Desktop"        
+o1 = FilesList(p)
+o1.listOfFiles()
+p = "C:\\Users\Humayun\Documents"
+o2 = FilesList(p)
+o2.listOfFiles()
+p = "C:\\SalesData"
+o3 = FilesList(p)
+o3.listOfFiles()
 
 def scanner(path):
     try:
